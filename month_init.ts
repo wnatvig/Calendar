841036255b8e0f2860e933acbca6c470c99ba249
hugo <hugo@bjornemo.se>
2025-02-25 15:48:01 +0100

    basic version of month_init() in month_init.ts, currectly does not write week_numbers and events_index

--- month_init.ts
+++ month_init.ts
@@ -1,5 +1,35 @@
 import * from 'time';
 
+function init_month(): Month {
+	let month: Month = {
+		year: 0,
+		month: 0,
+		month_length: 0,
+		first_weekday: 0,
+		week_numbers: [],
+		events_index: 0,
+	};
+
+	month.year = get_year();
+	month.month = get_month();
+	month.month_length = month_len(month.year, month.month);
+
+	let date = get_date();
+	let day = get_weekday();
+
+	while (date > 1) {
+		date--;
+		day--;
+		if (day === 0)
+			day = 7;
+	}
+
+	month.first_weekday = day;
+
+	return month;
+}
+
+
 const MONTH_LENGTHS: Array<number> = [
 	0,
 	31,
