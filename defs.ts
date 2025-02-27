2ee8cef81d7210c10b7a1a1aca0d995d14efea2c
Hugo B <hugo@bjornemo.se>
2025-02-27 16:43:50 +0100

    finished init_month() and added/moved things around in defs.ts and types.ts

--- defs.ts
+++ defs.ts
@@ -1,3 +1,11 @@
+export const MONDAY: number		= 1;
+export const TUESDAY: number	= 2;
+export const WEDNESDAY: number	= 3;
+export const THURSDAY: number	= 4;
+export const FRIDAY: number		= 5;
+export const SATURDAY: number	= 6;
+export const SUNDAY: number		= 7;
+
 export const NAMES_WEEKDAYS: Array<string> = [
 	"",
 	"Monday",
--- month_init.ts
+++ month_init.ts
@@ -1,8 +1,15 @@
 import * as td from './time_date';
-import type { Month } from './types';
-import { NAMES_WEEKDAYS, NAMES_MONTHS, MONTH_LENGTHS } from './defs';
+import type { Month, Event_list } from './types';
+import { NAMES_WEEKDAYS, NAMES_MONTHS, MONTH_LENGTHS,
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY } from './defs';
 
-function init_month(): Month {
+function init_month(eventlist: Event_list): Month {
 	let month: Month = {
 		year: 0,
 		month: 0,
@@ -12,13 +19,16 @@ function init_month(): Month {
 		events_index: 0,
 	};
 
+	// set year, month, and month_length
 	month.year = td.get_year();
 	month.month = td.get_month();
-	month.month_length = month_len(month.year, month.month);
+	month.month_length = month_length(month.year, month.month);
 
+	// current day and date
 	let date = td.get_date();
 	let day = td.get_weekday();
 
+	// loop back to date == 01 to get weekday of first day of month
 	while (date > 1) {
 		date--;
 		day--;
@@ -28,11 +38,100 @@ function init_month(): Month {
 
 	month.first_weekday = day;
 
+	load_weeks(month.year, month.month, month.first_weekday, month.week_numbers);
+	month.events_index = get_month_index(eventlist.base_year, eventlist.base_month, month.year, month.month);
+
 	return month;
 }
 
+function load_weeks(year: number, target_month: number, first_weekday: number, week_numbers: Array<number>): void {
 
-function month_len(year: number, month: number): number {
+	let month = target_month;
+	let day = first_weekday;
+	let date = 1;	// date corresponding to day
+
+	let week;
+
+	// loop back to january 1
+	while (month > 1) {
+		month--;
+		date = month_length(year, month);
+
+		day--;
+		if (day === 0)
+			day = 7;
+
+		while (date > 1) {
+			date--;
+			day--;
+			if (day === 0)
+				day = 7;
+		}
+	}
+	// now: date == 1, month == 1
+
+	// get first week based on what weekday january 1st is
+	if (day === MONDAY || day === TUESDAY || day === WEDNESDAY || day === THURSDAY) {
+		week = 1;
+	}
+	else if (day === FRIDAY) {
+		week = 53;
+	}
+	else if (day === SATURDAY && leap_year(year-1)) {
+		week = 53;
+	}
+	else {
+		week = 52;
+	}
+
+	// loop forward to target month
+	
+	let month_len;
+	let week_i = -1;
+
+	while (month <= target_month) {
+		month_len = month_length(year, month);
+		date = 1;
+
+		while (date <= month_len) {
+			if (day === MONDAY) {
+				if ((week === 52 || week === 53) && month == 1)
+					week = 1;
+				
+				else if ((week === 52 || week === 53) && date >= 29)
+					week = 1;
+
+				else
+					week++;
+			}
+
+			if (month === target_month && (week_numbers[week_i] != week || week_i === -1)) {
+				week_i++;
+				week_numbers[week_i] = week;
+			}
+
+			date++;
+			day++;
+			if (day === 8)
+				day = 1;
+		}
+
+		month++;
+	}
+}
+
+//TODO error/invalid/illegal input check
+export function get_month_index(base_year: number, base_month: number,
+						 year: number, month: number): number {
+	let year_diff = year - base_year;
+	let month_diff = month - base_month;
+
+	return year_diff * 12 + month_diff;
+}
+
+
+// get length of month (in days)
+function month_length(year: number, month: number): number {
 	if (month != 2)
 		return MONTH_LENGTHS[month];
 
@@ -42,6 +141,7 @@ function month_len(year: number, month: number): number {
 		return MONTH_LENGTHS[month];
 }
 
+//returns true if given year is a leap year, otherwise false
 function leap_year(year: number): boolean {
 	if (year % 4 === 0) {
 		if (year % 100 === 0) {
