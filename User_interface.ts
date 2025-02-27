39225390264f758618fcca2047bc43965f148093
David Creutziger <d.creutziger@gmail.com>
2025-02-27 15:22:02 +0100

    Added documentation to display_month and the ability for display_month to display days with events

--- User_interface.ts
+++ User_interface.ts
@@ -1,7 +1,9 @@
 
 import { Console } from "console";
 import { get_month, NAMES_MONTHS, NAMES_WEEKDAYS } from "./time";
-import { Month, Event_list, Event } from "./types";
+import { Month, Event_list, Event, get_month_index } from "./types";
+
+
 
 function divide_days_in_weeks(month:Month): Array<number>{
     let days_array:Array<number> = new Array(month.week_numbers.length * 7);
@@ -13,39 +15,79 @@ function divide_days_in_weeks(month:Month): Array<number>{
     return days_array;
 };
 
+function days_with_events(event_array: Array<Event>): Array<number> {
+    let event_days: Array<number> = [];
+    for (let i = 0; i <event_array.length; i++){
+        event_days[i] = event_array[i].day;
+    }
+    return event_days;
+};
+
 /**
- * Dispays a month in the terminal
- * @param month 
+ * Dispays a month in the terminal in a formatted way.
+ * The function prints:
+ * - The name of the month and year as a title.
+ * - A header with weekday abbreviations (Mon-Sun).
+ * - The days of the month, properly aligned under their respective weekday headers.
+ * - Week numbers displayed at the beginning of each row.
+ * @param {Month} month - the month to be displayed
+ * @param {Event_list} Event_list - The events for the calendar
  */
-export function display_month(month: Month): void{
+export function display_month(month: Month, Event_list: Event_list): void{    
     let day_array = divide_days_in_weeks(month);
-    let week_lenght = 7;
+    let week_end_index = 7;
+    //TODO: Add a function to show days when there are events
+
+    //Create an array with all days with events that month
+    let event_days = days_with_events(Event_list.events[month.events_index]);
+    
+
 
     console.log();
     console.log("          ",NAMES_MONTHS[month.month] ,month.year);
     console.log("   Mon Tue Wed Thu Fri Sat Sun")
     for (let weeks of month.week_numbers) {
-        process.stdout.write('' + weeks);
-        for(let i = week_lenght - 7; i < week_lenght; i++) {
+        process.stdout.write(`\x1b[31m${weeks}\x1b[0m`);
+        for(let i = week_end_index - 7; i < week_end_index; i++) {
             if (day_array[i] !== undefined){
-                if (day_array[i] < 10) {
-                    process.stdout.write('   ' + day_array[i]);
-                } else{
-                    process.stdout.write('  ' + day_array[i]);
+                if(event_days.includes(day_array[i])){
+                    if (day_array[i] < 10) {
+                        process.stdout.write('   ' + `\x1b[34m${day_array[i]}\x1b[0m`);
+                    } else{
+                        process.stdout.write('  ' + `\x1b[34m${day_array[i]}\x1b[0m`);
                 }
+                } else {
+                    if (day_array[i] < 10) {
+                        process.stdout.write('   ' + day_array[i]);
+                    } else{
+                        process.stdout.write('  ' + day_array[i]);
+                }
+                };
             } else {
                 process.stdout.write('    ');
             }
             
         }
-        week_lenght += 7;
+        week_end_index += 7;
         console.log();
 
     }
     console.log();
 };
 
+const event1: Event = {day: 22, 
+                       month: 1,
+                       year:2025,
+                       time_start:1,
+                       time_end: 1,
+                       description: "Tadläkare"};
 
+const event2: Event = {day: 5, 
+                       month: 1,
+                       year:2025,
+                       time_start:1,
+                       time_end: 1,
+                       description: "Tadläkare"};
 const month1: Month= {year: 2025, 
                     month: 1, 
                     month_length: 31, 
@@ -53,6 +95,6 @@ const month1: Month= {year: 2025,
                     week_numbers: [1, 2, 3, 4, 5], 
                     events_index: 0};
 
-display_month(month1);
+const Eent_array1: Event_list = {base_year:2025, base_month:1, events: [[event1, event2]]};
 
-
+display_month(month1, Eent_array1);
