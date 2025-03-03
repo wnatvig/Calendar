b18ac0624c5324655a9b4ddbe4c2f37809fd1561
Hugo B <hugo@bjornemo.se>
2025-03-03 15:55:50 +0100

    added add_event() and changed init_hashtable() and commented out other functions

--- hashtable.ts
+++ hashtable.ts
@@ -1,6 +1,9 @@
 import type { List, Pair } from './lib/list';
-import type { Hashtable, User } from './types';
+import type { Event, Event_list, Hashtable, User } from './types';
 import { pair, head, tail, is_null } from './lib/list';
+
+import { get_current_year, get_current_month } from './time_date';
+import { make_event_list, add_event_to_event_list } from './eventcreate';
 
 
 const HT_TABLE_SIZE: number = 100;
@@ -16,61 +19,93 @@ function simple_hash(str: string): number {
 	return hash;
 }
 
-//read file and initialize Array<User>		<- stupid idea
-//initialize hashtable from Array<User>		<- stupid idea
-//add event (use hashtable to get Array<User> index and add event, also append to file)
-//delete event (requires change in Array<User> and then total overwrite of file)
 
-//better than init_hashtable:
-//read file, for each line (1 event per line, complete with Event data + user):
-//    find user in hashtable, if user exists:
-//	      add event
-//	  else
-//	      add user
-//	      add event
+// delete event
+// delete user
 
 
-//discard this, probably
-export function init_hashtable(users: Array<User>): Hashtable {
+export function init_hashtable(): Hashtable {
 	let ht: Hashtable = {table: [], table_size: HT_TABLE_SIZE, hash: HT_HASH_FUNCTION};
 
 	for (let i = 0; i < ht.table_size; i++)
 		ht.table[i] = null;
 
-	for (let i = 0; i < users.length; i++)
-		ht_add_user(ht, users[i].username, i);
-
 	return ht;
 }
 
 
-function ht_add_user(ht: Hashtable, username: string, user_index: number): void {
-	if (ht_entry_exists(ht, username)) {
-		console.log(`DOUBLE ENTRY USER: ${username} -- SKIPPING`);
-		console.log("users: Array<User> IS INVALID");
-	} else {
-		let ht_i = ht.hash(username) % ht.table_size;
-		//ht.table[ht_i] = <List<Pair<string, number>>>pair(pair(username, user_index), ht.table[ht_i]);
-		ht.table[ht_i] = pair(pair(username, user_index), ht.table[ht_i]);
-	}
-}
+//TODO function spec
+//lookup username in hashtable
+//if user does not exist:
+//    create User in users at index i
+//    create ht entry for user with index i
+//    add event to User's (empty) Event_list
+//    base year/month will be set to event date if event < current, otherwise current date
+//if uses exists:
+//    retrieve user index from hashtable
+//    go to Array<User> at user index
+//    update user Event_list with new event
+//
+// if no event is given (optional parameter) user will be created but with an empty eventlist (current date as base)
+export function add_event(ht: Hashtable, users: Array<User>, username: string, event?: Event): void {
+	let ht_i = ht.hash(username) % ht.table_size;
+	let user_list: List<Pair<string, number>>;
+	let user_i;
 
-function ht_entry_exists(ht: Hashtable, key: string): boolean {
-	let lst: List<Pair<string, number>>;
-	let ht_i = ht.hash(key) % ht.table_size;
+	for (user_list = ht.table[ht_i]; !is_null(user_list); user_list = tail(user_list))
+		if (head(head(user_list)) === username)
+			break;
 
-	for (lst = ht.table[ht_i]; !is_null(lst); lst = tail(lst)) {
-		if (key === head(head(lst))) {
-			return true;
+	// head(user_list) == pair(username, user_index)	if user already in hashtable
+	// user_list == null	if user not in hashtable
+
+	if (user_list === null) {
+		user_i = users.length;
+
+		let base_year = get_current_year();
+		let base_month = get_current_month();
+
+		// if first event is earlier than current year/month -> set base year/month to that of event
+		// NOTE: this is only done when user_list === null, i.e. first call to add_event() for this specific user
+		if (event !== undefined && (event.year < base_year || (event.year === base_year && event.month < base_month))) {
+			base_year = event.year;
+			base_month = event.month;
 		}
+
+		users[user_i] = {username: username, eventlist: make_event_list(base_year, base_month, [])};
+
+		ht.table[ht_i] = pair(pair(username, user_i), ht.table[ht_i]);
+	} else {	// user found
+		user_i = tail(head(user_list));
 	}
-
-	return false;
+	
+	// add event to eventlist
+	// TODO IF GIVEN EVENT IS EARLIER THAN BASE THIS WILL GO BAD FIX FIX FIX
+	// THIS SHOULD NOT HAPPEN, ASSUMING VALID FILE AND NO ADDING EVENTS IN THE PAST
+	if (event !== undefined)
+		add_event_to_event_list(event, users[user_i].eventlist);
 }
 
-//TODO put somewhere else
-function add_user(user: User, users: Array<User>, ht: Hashtable) {
-	let i = users.length;
-	users[i] = user;
-	ht_add_user(ht, user.username, i);
-}
+//function ht_add_user(ht: Hashtable, username: string, user_index: number): void {
+//	if (ht_entry_exists(ht, username)) {
+//		console.log(`DOUBLE ENTRY USER: ${username} -- SKIPPING`);
+//		console.log("users: Array<User> IS INVALID");
+//	} else {
+//		let ht_i = ht.hash(username) % ht.table_size;
+//		//ht.table[ht_i] = <List<Pair<string, number>>>pair(pair(username, user_index), ht.table[ht_i]);
+//		ht.table[ht_i] = pair(pair(username, user_index), ht.table[ht_i]);
+//	}
+//}
+
+//function ht_entry_exists(ht: Hashtable, key: string): boolean {
+//	let lst: List<Pair<string, number>>;
+//	let ht_i = ht.hash(key) % ht.table_size;
+//
+//	for (lst = ht.table[ht_i]; !is_null(lst); lst = tail(lst)) {
+//		if (key === head(head(lst))) {
+//			return true;
+//		}
+//	}
+//
+//	return false;
+//}
