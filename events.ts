9bbc2607dd45603762edfbe6f6f4427180497049
William N <william@natvig.se>
2025-03-11 10:55:09 +0100

    Changed Documentation

--- events.ts
+++ events.ts
@@ -4,13 +4,13 @@ import { get_month_index } from './month'
 
 /**
  * Creates an Event object with the given date, time and description.
- * @param day - the day of the event (1 to 31)
- * @param month - the month of the event (1 to 12)
- * @param year - the year of the event (example: 2025)
- * @param time_start - the starting time of the event (example: 1200, for 12:00)
- * @param time_end - the ending time of the event (example: 1300, for 13:00)
- * @param description - a description of the event
- * @returns An event of type Event containing the specific details 
+ * @param {Number} day - the day of the event (1 to 31)
+ * @param {Number} month - the month of the event (1 to 12)
+ * @param {Number} year - the year of the event (example: 2025)
+ * @param {Number} time_start - the starting time of the event (example: 1200, for 12:00)
+ * @param {Number} time_end - the ending time of the event (example: 1300, for 13:00)
+ * @param {String} description - a description of the event
+ * @returns {Event} An event of type Event containing the specific details 
  */
 export function make_event(day: number, month: number, 
                     year: number, time_start: number, 
@@ -24,10 +24,10 @@ export function make_event(day: number, month: number,
 }
 /**
  * Creates an Event_list object that holds events for specific month and year.
- * @param base_year - The reference year for the event list 
- * @param base_month - the reference month for the event list
- * @param events - an array containing arrays where each inner array represents a month containing events
- * @returns an event list of type Event_list containing events that are associated with the given month and year
+ * @param {Number} base_year - The reference year for the event list 
+ * @param {Number} base_month - the reference month for the event list
+ * @param {Event_list} events - an array containing arrays where each inner array represents a month containing events
+ * @returns {Event_list} an event list of type Event_list containing events that are associated with the given month and year
  */
 export function make_event_list(base_year: number,
 	                            base_month: number,
@@ -42,9 +42,9 @@ export function make_event_list(base_year: number,
 
 /**
  * Adds an event to the correct posision in the given Event_list
- * @param event - the event added
- * @param event_list - the list of events that the event gets added into
- * @returns 0 if the event was added and 1 of the event month is out of range
+ * @param {Event} event - the event added
+ * @param {Event_list} event_list - the list of events that the event gets added into
+ * @returns {Number} 0 if the event was added and 1 of the event month is out of range
  */
 export function add_event_to_event_list(event: Event, event_list: Event_list): number {
     const month_index = get_month_index(event_list.base_year, event_list.base_month, 
@@ -63,8 +63,8 @@ export function add_event_to_event_list(event: Event, event_list: Event_list): n
 
 /**
  * Removes a specific event from the given Event_list
- * @param event - the event being removed
- * @param event_list - the list of events 
+ * @param {Event} event - the event being removed
+ * @param {Event_list} event_list - the list of events 
  * @returns 0 if the event was removed, 
  * 			1 if the event month is out of range,
  * 			2 if the event does not exist
